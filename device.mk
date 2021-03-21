de05cc63414bcbd28f14e296e31bba36be5c6ef6
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 19:31:35 -0700

    op9: Build the QTI health HAL.
    Change-Id: I86abeb45119d1cdf05dfb1fd28090628cee64925

--- device.mk
+++ device.mk
@@ -100,6 +100,11 @@ LOC_HIDL_VERSION = 4.0
 # GSI
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-service
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
